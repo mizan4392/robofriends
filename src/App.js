54e8b888ec5cfb738e45745be47188ee765231bc
mizan4392 <md.mizan4392@gmail.com>
2019-05-25 09:40:30 +0600

    Header,SearchBoxand Spinner Added and added lifecyclehooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import './App.css';
-import { robots } from './robots';
+import Spinner from './components/Spinner/Spinner';
 import CardList from './components/CardList/CardList';
-function App() {
-  return (
-    <div className='App'>
-      <CardList robots={robots}/>
-    </div>
-  );
+import SearchBox from './components/SearchBox/SearchBox';
+
+
+class App extends React.Component {
+
+  state = {
+    robots:[],
+    searchFild:''
+  }
+
+  componentDidMount(){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({robots:users}));
+
+   
+  }
+
+  onSearchChange = (event) =>{
+    
+    this.setState({searchFild:event.target.value})
+
+  }
+
+  render(){
+    
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLocaleLowerCase().includes(this.state.searchFild.toLocaleLowerCase());
+    })
+
+    if(this.state.robots.length === 0){
+      return <Spinner />
+    }else{
+      return (
+        <div className='App'>
+          <h1 className='header'>ROBOFRIENDS</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots}/>
+        </div>
+      );
+    }
+
+    
+}
+
 }
 
 export default App;
